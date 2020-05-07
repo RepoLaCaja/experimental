1ab909886530a0c06d313bc9a6acfcb738cfabbc
anamaria.zuluaga <anamaria.zuluaga@AR-NB-329.mshome.net>
2020-05-07 16:49:37 -0300

    test

---
+++ Test Suites/test APISReport.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test APISReport</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>96a485fc-befb-4b57-b74e-3a3136d6ae24</testSuiteGuid>
+   <testCaseLink>
+      <guid>bcce86eb-dbe5-4a3b-a7c5-599e9ff59667</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CreateAndVerfyPUT</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>da013a9b-3cca-4b03-a5f0-ca499edf4b99</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VerifyRestService</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>809d7760-62b4-41fb-886d-68cc78a2b481</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VerifyDELETE</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
